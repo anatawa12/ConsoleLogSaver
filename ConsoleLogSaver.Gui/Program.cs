ca5589d3a94c41e967c5c7fd1244580f30107bce
anatawa12 <anatawa12@icloud.com>
2023-05-28 21:06:21 +0900

    feat: implement window

--- ConsoleLogSaver.Gui/Program.cs
+++ ConsoleLogSaver.Gui/Program.cs
@@ -1,10 +1,167 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Anatawa12.ConsoleLogSaver;
+using Mono.Debugger.Soft;
 
-var form = new Form();
+sealed class MainWindow : Form
+{
+    private ConsoleLogSaver _saver = new();
+    public ListView UnityInstances { get; }
 
-form.Width = 200;
-form.Height = 200;
-form.Text = "Sample";
+    // ReSharper disable once MemberInitializerValueIgnored
+    public Button[] SelectionRequiredButtons { get; } = Array.Empty<Button>();
 
-Application.Run(form);
+    [STAThread]
+    public static void Main()
+    {
+        Application.Run(new MainWindow());
+    }
+
+    public MainWindow()
+    {
+        FormBorderStyle = FormBorderStyle.Fixed3D;
+        Width = 400;
+        Height = 400;
+        Text = "Console Log Saver";
+
+        UnityInstances = AddControl(95, new ListView
+        {
+            View = View.Details,
+            Scrollable = true,
+            MultiSelect = false,
+            Columns =
+            {
+                new ColumnHeader { Text = "PID", Width = -2 },
+                new ColumnHeader { Text = "Project Path", Width = -2 },
+            },
+        });
+        UnityInstances.SelectedIndexChanged += (_, _) => SetButtonEnabled();
+
+        AddButton("Reload Unity List", (_, _) => ReloadUnity());
+
+        AddControl(new Label { Text = "Security Settings" }, 15);
+
+        AddControl(new CheckBox { Text = "Unity Version", Checked = true, Enabled = false }, 15);
+
+        void FieldCheckBox(string text, bool @checked, Action<bool> setter)
+        {
+            var box = AddControl(new CheckBox { Text = text, Checked = @checked }, 15);
+            box.CheckedChanged += (_, _) => setter(box.Checked);
+        }
+
+        FieldCheckBox("Hide OS Info", _saver.HideOsInfo, v => _saver.HideOsInfo = v);
+        FieldCheckBox("Hide User Name", _saver.HideUserName, v => _saver.HideUserName = v);
+        FieldCheckBox("Hide User Home", _saver.HideUserHome, v => _saver.HideUserHome = v);
+
+        SelectionRequiredButtons = new[]
+        {
+            AddButton("Save to File", SaveToFile, false),
+            AddButton("Copy to Clipboard", CopyToClipboard, false),
+        };
+
+        ReloadUnity();
+    }
+
+    private void SetButtonEnabled()
+    {
+        var enabled = UnityInstances.SelectedItems.Count != 0;
+
+        foreach (var button in SelectionRequiredButtons)
+            button.Enabled = enabled;
+    }
+
+    private int _yPosition = 10;
+
+    private Button AddButton(string text, EventHandler handler, bool enabled = true)
+    {
+        var generate = AddControl(new Button(), 30);
+        generate.Text = text;
+        generate.Click += handler;
+        generate.Enabled = enabled;
+        return generate;
+    }
+
+    private T AddControl<T>(T control, int height) where T : Control => AddControl(height, control);
+
+    private T AddControl<T>(int height, T control) where T : Control
+    {
+        const int width = 360;
+        control.Location = new Point(10, _yPosition);
+        control.Size = new Size(width, height);
+        Controls.Add(control);
+        _yPosition += height + 10;
+        return control;
+    }
+
+    private async void ReloadUnity()
+    {
+        foreach (ListViewItem item in UnityInstances.Items)
+        {
+            try
+            {
+                (item as UnitySessionItem)?.Session?.Dispose();
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        UnityInstances.Items.Clear();
+        SetButtonEnabled();
+
+        var processes = await DebuggerSession.ConnectAllUnityProcesses(TimeSpan.FromSeconds(2));
+
+        UnityInstances.Items.AddRange(processes.Select(s => new UnitySessionItem(s)).ToArray<ListViewItem>());
+        SetButtonEnabled();
+    }
+
+    private async Task<ConsoleLogFileV1?> CollectData()
+    {
+        try
+        {
+            var item = (UnitySessionItem)UnityInstances.SelectedItems[0];
+            return await _saver.Collect(item.Session);
+        }
+        catch (VMDisconnectedException)
+        {
+            MessageBox.Show("The Unity Process exited.", "ERROR");
+            return null;
+        }
+    }
+
+    private async void SaveToFile(object? sender, EventArgs e)
+    {
+        var openFile = new SaveFileDialog
+        {
+            Title = "Save To File",
+            FileName = "logfile.txt",
+            Filter = "Text files (*.txt)|*.txt",
+        };
+
+        if (openFile.ShowDialog() == DialogResult.OK)
+        {
+            var collect = await CollectData();
+            if (collect == null) return;
+            var asText = LogFileWriter.WriteToString(collect);
+            using var writer = new StreamWriter(openFile.OpenFile());
+            await writer.WriteAsync(asText);
+        }
+    }
+
+    private async void CopyToClipboard(object? sender, EventArgs e)
+    {
+        var collect = await CollectData();
+        if (collect == null) return;
+        Clipboard.SetText(LogFileWriter.WriteToString(collect));
+        MessageBox.Show("Copied", "Copied!");
+    }
+
+    class UnitySessionItem : ListViewItem
+    {
+        public UnitySessionItem(DebuggerSession session) : base(new[] { session.Pid.ToString(), session.ProjectRoot })
+        {
+            Session = session;
+        }
+
+        public DebuggerSession Session { get; }
+    }
+}

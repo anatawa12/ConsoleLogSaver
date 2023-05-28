643ed04f56769080fb0e7b59e3170ce9403fdd30
anatawa12 <github@anatawa12.com>
2023-05-28 17:45:50 +0900

    feat: initial commit for windows gui with forms

---
+++ ConsoleLogSaver.Gui/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var form = new Form();
+
+form.Width = 200;
+form.Height = 200;
+form.Text = "Sample";
+
+Application.Run(form);

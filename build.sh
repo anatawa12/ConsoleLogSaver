718c3077cb4bbdef9d2e77a6323fac3a40e991a6
anatawa12 <anatawa12@icloud.com>
2023-12-13 20:10:26 +0900

    chore: use dotnet 8.0

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -eu
 rm -rf bin
 mkdir -p bin
 
+FRAMEWORK=net8.0
+
 # utils
 
 cp_if() {
@@ -15,7 +17,7 @@ cp_if() {
 build_cli() {
   for target in osx-arm64 osx-x64 win-x64; do
     dotnet publish ./ConsoleLogSaver.Cli/ConsoleLogSaver.Cli.csproj -r "$target" -c:Release
-    PUBLISH="./ConsoleLogSaver.Cli/bin/Release/net6.0/$target/publish"
+    PUBLISH="./ConsoleLogSaver.Cli/bin/Release/$FRAMEWORK/$target/publish"
     cp_if "$PUBLISH/ConsoleLogSaver.Cli"     "bin/ConsoleLogSaver.Cli-$target"
     cp_if "$PUBLISH/ConsoleLogSaver.Cli.exe" "bin/ConsoleLogSaver.Cli-$target.exe"
   done
@@ -23,12 +25,10 @@ build_cli() {
 
 build_gui() {
   target=win-x64
-  PUBLISH="./ConsoleLogSaver.Gui/bin/Release/net6.0-windows/$target/publish"
+  PUBLISH="./ConsoleLogSaver.Gui/bin/Release/$FRAMEWORK-windows/$target/publish"
   rm -rf "${PUBLISH:?}/bin" "${PUBLISH:?}/ConsoleLogSaver.Gui.zip"
 
   dotnet publish ./ConsoleLogSaver.Gui/ConsoleLogSaver.Gui.csproj -c:Release
-  # because of https://github.com/dotnet/runtime/issues/3828, we need to fix subsystem of PE file
-  dotnet run --project PEFlagSetter "$PUBLISH/ConsoleLogSaver.Gui.exe"
 
   mkdir -p "$PUBLISH/bin"
   mv "$PUBLISH"/*.exe "$PUBLISH"/*.dll "$PUBLISH/bin"

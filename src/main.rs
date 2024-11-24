059d1215c25594f13fa6614c89c31355956d9228
anatawa12 <anatawa12@icloud.com>
2024-11-24 20:53:09 +0900

    fix: not working on macos

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use bytemuck::{AnyBitPattern, NoUninit};
-use lldb::{lldb_addr_t, lldb_offset_t, lldb_pid_t, ByteOrder, FunctionNameType, SBAddress, SBAttachInfo, SBData, SBDebugger, SBError, SBExpressionOptions, SBFileSpec, SBFrame, SBListener, SBModule, SBModuleSpec, SBProcess, SBSection, SBSymbol, SBTarget, SBValue, SymbolType};
+use lldb::{lldb_addr_t, lldb_offset_t, lldb_pid_t, ByteOrder, FunctionNameType, Permissions, SBAddress, SBAttachInfo, SBData, SBDebugger, SBError, SBExpressionOptions, SBFileSpec, SBFrame, SBListener, SBModule, SBModuleSpec, SBProcess, SBSection, SBSymbol, SBTarget, SBValue, SymbolType};
 use std::env::args;
+use std::ffi::CStr;
 use std::io::Write;
 use std::marker::PhantomData;
 use std::process::{exit, Stdio};
 use std::thread::sleep;
 use std::time::{Duration, Instant};
-use byteorder::{NativeEndian, ReadBytesExt};
+use byteorder::{NativeEndian, ReadBytesExt, WriteBytesExt};
 
 fn main() {
     let mut args = args();
@@ -133,8 +134,18 @@ fn main() {
 
     let ctx = LLDBContext::new(&target, &process, &frame);
 
-    {
-        let path = attach_lib_dylib_path.to_str().unwrap();
+    struct LoadImageResult<F> {
+        saver_save: lldb_addr_t,
+        location: lldb_addr_t,
+        unload: F,
+    }
+
+    #[cfg(windows)]
+    fn load_image(process: &SBProcess, load_path: &std::path::Path) -> Result<LoadImageResult<impl FnOnce()>, SBError> {
+        // on windows, we can find_module for modules we just loaded, so we use 
+        let target = process.target().unwrap();
+
+        let path = load_path.to_str().unwrap();
         let dylib = SBFileSpec::from_path(path);
         let image_token = process.load_image(&dylib).expect("loading image");
 
@@ -157,6 +168,266 @@ fn main() {
             .start_address()
             .unwrap()
             .load_address(&target);
+
+        let process = process.clone();
+        Ok(LoadImageResult {
+            saver_save,
+            location,
+            unload: move || {
+                process.unload_image(image_token).expect("unloading image");
+            }
+        })
+    }
+
+    #[cfg(unix)]
+    fn load_image(process: &SBProcess, load_path: &std::path::Path) -> Result<LoadImageResult<impl FnOnce()>, SBError> {
+        use std::os::unix::ffi::OsStrExt;
+        // on unix, we cannot find_module for modules we just loaded,
+        // so we use directly calling dlopen 
+        let frame = process.selected_thread().frames().nth(0).unwrap();
+
+        /*
+        C-like expression:
+        struct InOut {
+          // input data
+          const char *load_path, // 0
+          // constants. For easier writing code, we pass constants with inoput struct
+          const char *saver_save_name, // 1
+          const char *location_name, // 2
+          // error data.
+          const char *error, // 3
+          size_t error_len, // 4
+          // success data
+          void *handle, // 5
+          void *saver_save, // 6
+          void *location, // 7
+        }
+
+        #define RTLD_LAZY 1
+        InOut *input = <pointer>;
+
+        input.handle = void *handle = dlopen(input->load_path, RTLD_LAZY);
+        if (handle == NULL) goto error;
+
+        const char *saver_save_name = input->saver_save_name;
+        input.saver_save = void *saver_save = dlsym(handle, saver_save_name);
+        if (saver_save == NULL) goto error;
+
+        const char *location_name = input->location_name;
+        input.location   = void *location   = dlsym(handle, location_name);
+        if (location == NULL) goto error;
+
+        goto return;
+       error:
+        input.error = void *error = dlerror();
+        if (error != null) input.error_len = strlen(error);
+        if (handle != null) dlclose(handle);
+        goto return;
+       return:
+        return;
+         */
+
+        let load_path_idx: usize = 0;
+        let saver_save_name_idx: usize = 1;
+        let location_name_idx: usize = 2;
+        let error_idx: usize = 3;
+        let error_len_idx: usize = 4;
+        let handle_idx: usize = 5;
+        let saver_save_idx: usize = 6;
+        let location_idx: usize = 7;
+        const INOUT_ELEMENT_COUNT: usize = 8;
+
+        let saver_save_name = "CONSOLE_LOG_SAVER_SAVE";
+        let location_name = "CONSOLE_LOG_SAVER_SAVED_LOCATION";
+
+        const STRUCT_DATA_SIZE: usize = size_of::<usize>() * INOUT_ELEMENT_COUNT;
+
+        let mut buffer = Vec::with_capacity(STRUCT_DATA_SIZE
+            + (load_path.as_os_str().len() + 1)
+            + (saver_save_name.len() + 1)
+            + (location_name.len() + 1));
+        buffer.extend_from_slice(&[0u8; STRUCT_DATA_SIZE]);
+        let mut buffer_writer = std::io::Cursor::new(&mut buffer);
+
+        fn write_get_ptr(writer: &mut std::io::Cursor<&mut Vec<u8>>, data: &[u8]) -> u64 {
+            let offset = writer.position();
+            writer.write_all(data).unwrap();
+            writer.write_all(b"\0").unwrap();
+            offset
+        }
+
+        buffer_writer.set_position(STRUCT_DATA_SIZE as u64);
+        let load_path_offset = write_get_ptr(&mut buffer_writer, &load_path.as_os_str().as_bytes());
+        let saver_save_name_offset = write_get_ptr(&mut buffer_writer, &saver_save_name.as_bytes());
+        let location_name_offset = write_get_ptr(&mut buffer_writer, &location_name.as_bytes());
+
+        let buffer_location = process.allocate_memory(buffer.len(), Permissions::READABLE | Permissions::WRITABLE).expect("allocating memory");
+
+        fn set_usize(buffer: &mut Vec<u8>, index: usize, value: usize) {
+            let offset = index * size_of::<usize>();
+            buffer[offset..][..size_of::<usize>()].copy_from_slice(&value.to_ne_bytes());
+        }
+
+        set_usize(&mut buffer, load_path_idx, (buffer_location + load_path_offset) as usize);
+        set_usize(&mut buffer, saver_save_name_idx, (buffer_location + saver_save_name_offset) as usize);
+        set_usize(&mut buffer, location_name_idx, (buffer_location + location_name_offset) as usize);
+
+        process.write_memory(buffer_location, &buffer).expect("writing memory");
+        drop(buffer);
+
+        let error_block = 3;
+        let ok_block = 7;
+
+        let expression = format!(r#"
+#!mini-llvm-expr 8
+define_struct InOut ptr ptr ptr ptr iptr ptr ptr ptr
+; functions
+; declare ptr @dlopen(ptr, i32)
+define_function_type ptr dlopen ptr i32
+declare_function dlopen dlopen
+
+; declare ptr @dlerror()
+define_function_type ptr dlerror
+declare_function dlerror dlerror
+
+; declare ptr @dlsym(ptr, ptr)
+define_function_type ptr dlsym ptr ptr
+declare_function dlsym dlsym
+
+; declare iptr @strlen(ptr)
+define_function_type iptr strlen ptr
+declare_function strlen strlen
+
+; declare i32 @dlclose(ptr)
+define_function_type i32 dlclose ptr
+declare_function dlclose dlclose
+; constants
+const RTLD_LAZY i32 1
+const iptr_0 iptr 0
+const load_path_idx i32 {load_path_idx}
+const saver_save_name_idx i32 {saver_save_name_idx}
+const location_name_idx i32 {location_name_idx}
+const error_idx i32 {error_idx}
+const error_len_idx i32 {error_len_idx}
+const handle_idx i32 {handle_idx}
+const saver_save_idx i32 {saver_save_idx}
+const location_idx i32 {location_idx}
+const input ptr {buffer_location}
+
+const iptr_1 iptr 1
+const iptr_2 iptr 2
+const iptr_3 iptr 3
+
+; main expression
+begin_block 0
+  load load_path ptr input
+  call handle dlopen dlopen load_path RTLD_LAZY
+  getelementptr handle_ptr InOut input iptr_0 handle_idx
+  store handle handle_ptr
+  icmp handle_is_null_0 eq handle null
+  cond_br handle_is_null_0 {error_block} 1
+  
+begin_block 1
+  getelementptr saver_save_name_ptr InOut input iptr_0 saver_save_name_idx
+  load saver_save_name ptr saver_save_name_ptr
+  call saver_save dlsym dlsym handle saver_save_name
+  getelementptr saver_save_ptr InOut input iptr_0 saver_save_idx
+  store saver_save saver_save_ptr
+  icmp saver_save_is_null eq saver_save null
+  cond_br saver_save_is_null {error_block} 2
+  
+begin_block 2
+  getelementptr location_name_ptr InOut input iptr_0 location_name_idx
+  load location_name ptr location_name_ptr
+  call location dlsym dlsym handle location_name
+  getelementptr location_ptr InOut input iptr_0 location_idx
+  store location location_ptr
+  icmp location_is_null eq location null
+  cond_br location_is_null {error_block} {ok_block}
+
+begin_block 3 # error_block
+  call error dlerror dlerror
+  getelementptr error_ptr InOut input iptr_0 error_idx
+  store error error_ptr
+  icmp error_is_null eq error null
+  cond_br error_is_null 5 4
+
+begin_block 4
+  call error_len strlen strlen error
+  getelementptr error_len_ptr InOut input iptr_0 error_len_idx
+  store error_len error_len_ptr
+  br 5
+
+begin_block 5
+  icmp handle_is_null_2 eq handle null
+  cond_br handle_is_null_2 7 6
+begin_block 6
+  call _ dlclose dlclose handle
+  br 7
+
+begin_block 7 # ok_block
+  ret_void
+"#);
+
+        let options = SBExpressionOptions::new();
+        let result = frame.evaluate_expression(&expression, &options);
+
+        let mut read_buffer = [0usize; INOUT_ELEMENT_COUNT];
+        process.read_memory(buffer_location, bytemuck::cast_slice_mut(&mut read_buffer)).expect("reading memory");
+
+        // 0x1001 is kNoResult, which is not an error
+        // https://github.com/llvm/llvm-project/blob/d6e65a66095cc3c93ea78669bc41d0885780e8ea/lldb/include/lldb/Expression/UserExpression.h#L274
+        if result
+            .error()
+            .map(|x| x.is_failure() && x.error() != 0x1001)
+            .unwrap_or(false)
+        {
+            panic!("{}", result.error().unwrap());
+        }
+
+        let handle = read_buffer[handle_idx];
+        let saver_save = read_buffer[saver_save_idx] as lldb_addr_t;
+        let location = read_buffer[location_idx] as lldb_addr_t;
+        let error = read_buffer[error_idx];
+
+        if error != 0 || handle == 0 || saver_save == 0 || location == 0 {
+            if error != 0 {
+                // there is error message from dlerror
+                let error_len = read_buffer[error_len_idx];
+                let mut error_buffer = vec![0u8; error_len + 1];
+                process.read_memory(error as lldb_addr_t, &mut error_buffer).expect("reading error message");
+                let error_message = CStr::from_bytes_with_nul(&error_buffer).expect("bad error msssage");
+                let message = error_message.to_str().expect("bad utf8 message");
+                panic!("dlopen or dlsym failed with error: {message}")
+            } else {
+                panic!("dlopen or dlsym failed with unknown error")
+            }
+        }
+
+        let unload = move || {
+            let options = SBExpressionOptions::new();
+            frame.evaluate_expression(&format!(r#"
+#!mini-llvm-expr 1
+define_function_type i32 dlclose ptr
+declare_function dlclose dlclose
+
+const target_ptr ptr {handle}
+call ret dlclose dlclose target_ptr
+ret_void"#), &options);
+        };
+
+        Ok(LoadImageResult {
+            saver_save,
+            location,
+            unload,
+        })
+    }
+
+    {
+        let load_image = load_image(&process, attach_lib_dylib_path.as_ref()).expect("load_image");
+
+        let saver_save = load_image.saver_save;
+        let location = load_image.location;
 
         println!("saver save address: {}", saver_save);
         println!("saver location address: {}", location);
@@ -196,10 +467,9 @@ fn main() {
             }
         } else {
             println!("version mismatch ({version})");
-            std::fs::write("received.bin", buffer).ok();
         }
 
-        process.unload_image(image_token).expect("unloading image");
+        (load_image.unload)();
     }
 
     // I don't know why but detaching with synchronous and no resume 
@@ -319,6 +589,24 @@ unsafe trait SBProcessExt {
         }
     }
 
+    fn write_memory(&self, addr: lldb_addr_t, buffer: &[u8]) -> Result<(), SBError> {
+        unsafe {
+            let error = SBError::default();
+            lldb::sys::SBProcessWriteMemory(
+                self.raw(),
+                addr,
+                buffer.as_ptr() as *mut _,
+                buffer.len(),
+                error.raw,
+            );
+            if error.is_success() {
+                Ok(())
+            } else {
+                Err(error)
+            }
+        }
+    }
+
     fn byte_roder(&self) -> ByteOrder {
         unsafe { lldb::sys::SBProcessGetByteOrder(self.raw()) }
     }
@@ -343,6 +631,18 @@ unsafe trait SBProcessExt {
                 Err(error)
             } else {
                 Ok(())
+            }
+        }
+    }
+
+    fn target(&self) -> Option<SBTarget> {
+        unsafe {
+            let raw = lldb::sys::SBProcessGetTarget(self.raw());
+            let target = SBTarget { raw };
+            if target.is_valid() {
+                Some(target)
+            } else {
+                None
             }
         }
     }

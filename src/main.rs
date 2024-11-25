c5fe9a2b659ed8b8ae8ac8568393a89a805a035d
anatawa12 <anatawa12@icloud.com>
2024-11-25 16:09:53 +0900

    feat: split function for CLS core

--- src/main.rs
+++ src/main.rs
@@ -15,51 +15,55 @@ fn main() {
         .parse::<process_remote::ProcessId>()
         .expect("Failed to parse unity pid");
 
-    let buffer = process_remote::get_buffer(unity_pid).expect("Failed to get buffer");
+    print!("{}", run_console_log_saver(unity_pid));
+}
+
+fn run_console_log_saver(pid: process_remote::ProcessId) -> String {
+    let buffer = process_remote::get_buffer(pid).expect("Failed to get buffer");
 
     let mut reader = TransferDataReader::new(buffer);
 
     let version: i32 = reader.read_i32();
-    if version == 1 {
-        let mut cls_file_builder = ClsFileBuilder::new();
-        cls_file_builder.add_header(
-            "Vendor",
-            concat!(
-                "ConsoleLogSaver/",
-                env!("CARGO_PKG_VERSION"),
-                " (CLS-LLDB-RS)"
-            ),
-        );
-
-        let unity_version = reader.read_string();
-        cls_file_builder.add_header("Unity-Version", &unity_version);
-
-        let os_description = reader.read_string();
-        cls_file_builder.add_header("Editor-Platform", &os_description);
-
-        let build_target = reader.read_string();
-        cls_file_builder.add_header("Build-Target", &build_target);
-
-        let current_directory = reader.read_string();
-
-        append_upm(&mut cls_file_builder, &current_directory);
-        append_vpm(&mut cls_file_builder, &current_directory);
-
-        let mut cls_file_builder = cls_file_builder.begin_body();
-
-        let length: i32 = reader.read_i32();
-        for _ in 0..length {
-            let log_message = reader.read_string();
-            let mode = reader.read_i32();
-            cls_file_builder.add_header("Mode", &format!("{mode}")); // TODO: transfer to name
-            cls_file_builder.add_header("Mode-Raw", &format!("{mode:08x}"));
-            cls_file_builder.add_content("log-element", &log_message);
-        }
-
-        print!("{}", cls_file_builder.build());
-    } else {
-        eprintln!("version mismatch ({version})");
+    if version != 1 {
+        panic!("version mismatch ({version})");
     }
+
+    let mut cls_file_builder = ClsFileBuilder::new();
+    cls_file_builder.add_header(
+        "Vendor",
+        concat!(
+        "ConsoleLogSaver/",
+        env!("CARGO_PKG_VERSION"),
+        " (CLS-LLDB-RS)"
+        ),
+    );
+
+    let unity_version = reader.read_string();
+    cls_file_builder.add_header("Unity-Version", &unity_version);
+
+    let os_description = reader.read_string();
+    cls_file_builder.add_header("Editor-Platform", &os_description);
+
+    let build_target = reader.read_string();
+    cls_file_builder.add_header("Build-Target", &build_target);
+
+    let current_directory = reader.read_string();
+
+    append_upm(&mut cls_file_builder, &current_directory);
+    append_vpm(&mut cls_file_builder, &current_directory);
+
+    let mut cls_file_builder = cls_file_builder.begin_body();
+
+    let length: i32 = reader.read_i32();
+    for _ in 0..length {
+        let log_message = reader.read_string();
+        let mode = reader.read_i32();
+        cls_file_builder.add_header("Mode", &format!("{mode}")); // TODO: transfer to name
+        cls_file_builder.add_header("Mode-Raw", &format!("{mode:08x}"));
+        cls_file_builder.add_content("log-element", &log_message);
+    }
+
+    cls_file_builder.build()
 }
 
 struct TransferDataReader {

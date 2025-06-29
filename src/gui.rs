d43d3a103a8c73556317f34408d1779023aaa123
anatawa12 <anatawa12@icloud.com>
2025-06-29 17:12:26 +0900

    chore: move everything to main function

--- src/gui.rs
+++ src/gui.rs
@@ -1,13 +1,12 @@
 #![windows_subsystem = "windows"]
 
 use libui::controls::{
-    Button, Checkbox, Combobox, Group, Label, ProgressBar, ProgressBarValue, SelectionMode, Table,
+    Button, Checkbox, Group, Label, ProgressBar, ProgressBarValue, SelectionMode, Table,
     TableDataSource, TableModel, TableParameters, TableValue, TableValueType, VerticalBox,
 };
 use libui::prelude::*;
 use std::cell::RefCell;
 use std::rc::Rc;
-use std::thread;
 
 fn main() {
     let ui = UI::init().expect("Couldn't initialize UI library");
@@ -22,41 +21,66 @@ fn main() {
 
     let data = Rc::new(RefCell::new(UnityProcessList::new()));
     let table_model = Rc::new(RefCell::new(TableModel::new(data.clone())));
-    let layout = UILayout::new(table_model.clone());
 
-    {
-        let layout_rc = &layout;
-        let mut layout = layout.borrow_mut();
+    let mut vbox = VerticalBox::new();
+    vbox.set_padded(true);
 
-        layout.do_break.on_clicked({
-            let layout_weak = Rc::downgrade(layout_rc);
-            let ui = ui.clone();
-            move |_| {
-                let Some(layout) = layout_weak.upgrade() else {
-                    return;
-                };
-                let mut layout = layout.borrow_mut();
+    let parameters = TableParameters::new(table_model);
+    let mut table = Table::new(parameters);
+    table.append_text_column("pid", 0, Table::COLUMN_READONLY);
+    table.set_selection_mode(SelectionMode::ZeroOrOne);
+    vbox.append(table.clone(), LayoutStrategy::Stretchy);
 
-                layout.start_fetch();
-                thread::spawn({
-                    let queue = libui::EventQueueWithData::new(&ui, layout_weak.clone());
-                    move || {
-                        std::thread::sleep(std::time::Duration::from_secs(3));
-                        queue.queue_main(|layout| {
-                            let Some(layout) = layout.upgrade() else {
-                                return;
-                            };
-                            let mut layout = layout.borrow_mut();
-                            layout.finish_fetch("Finished");
-                        })
-                    }
-                });
-            }
-        });
-    }
+    let version_info = Label::new("A label");
+    vbox.append(version_info.clone(), LayoutStrategy::Compact);
+
+    let download_latest_version = Button::new("The Button");
+    vbox.append(download_latest_version.clone(), LayoutStrategy::Compact);
+
+    let mut security_settings_box = VerticalBox::new();
+    let mut security_settings_group = Group::new("Group");
+
+    let mut unity_version_required = Checkbox::new("Checkbox");
+    unity_version_required.disable();
+    security_settings_box.append(unity_version_required.clone(), LayoutStrategy::Compact);
+
+    security_settings_group.set_child(security_settings_box);
+    vbox.append(security_settings_group.clone(), LayoutStrategy::Compact);
+
+    let progress_txt = Label::new("");
+    vbox.append(progress_txt.clone(), LayoutStrategy::Compact);
+
+    let mut progress_bar = ProgressBar::new();
+    progress_bar.hide();
+    progress_bar.set_value(ProgressBarValue::Indeterminate);
+    vbox.append(progress_bar.clone(), LayoutStrategy::Compact);
+
+    let mut do_break = Button::new("Do Break");
+    vbox.append(do_break.clone(), LayoutStrategy::Compact);
+
+    do_break.on_clicked({
+        let ui = ui.clone();
+        let mut progress_txt = progress_txt.clone();
+        let mut progress_bar = progress_bar.clone();
+        move |_| {
+            progress_txt.set_text("1st\n2nd");
+            progress_txt.show();
+            progress_bar.show();
+            std::thread::spawn({
+                let queue = libui::EventQueueWithData::new(&ui, (progress_txt.clone(), progress_bar.clone()));
+                move || {
+                    std::thread::sleep(std::time::Duration::from_secs(3));
+                    queue.queue_main(|(progress_txt, progress_bar)| {
+                        progress_txt.clone().set_text("Finished");
+                        progress_bar.clone().hide();
+                    })
+                }
+            });
+        }
+    });
 
     // Actually put the button in the window
-    win.set_child(layout.borrow().vbox.clone());
+    win.set_child(vbox.clone());
 
     // Show the window
     win.show();
@@ -96,74 +120,5 @@ impl TableDataSource for UnityProcessList {
 
     fn set_cell(&mut self, _: i32, _: i32, _: TableValue) {
         // unsupported
-    }
-}
-
-struct UILayout {
-    do_break: Button,
-    vbox: VerticalBox,
-    progress_txt: Label,
-    progress_bar: ProgressBar,
-}
-
-impl UILayout {
-    fn new(table_model: Rc<RefCell<TableModel>>) -> Rc<RefCell<Self>> {
-        let result = Rc::<RefCell<Self>>::new({
-            let mut vbox = VerticalBox::new();
-            vbox.set_padded(true);
-
-            let parameters = TableParameters::new(table_model);
-            let mut table = Table::new(parameters);
-            table.append_text_column("pid", 0, Table::COLUMN_READONLY);
-            table.set_selection_mode(SelectionMode::ZeroOrOne);
-            vbox.append(table.clone(), LayoutStrategy::Stretchy);
-
-            let version_info = Label::new("A label");
-            vbox.append(version_info.clone(), LayoutStrategy::Compact);
-
-            let download_latest_version = Button::new("The Button");
-            vbox.append(download_latest_version.clone(), LayoutStrategy::Compact);
-
-            let mut security_settings_box = VerticalBox::new();
-            let mut security_settings_group = Group::new("Group");
-
-            let mut unity_version_required = Checkbox::new("Checkbox");
-            unity_version_required.disable();
-            security_settings_box.append(unity_version_required.clone(), LayoutStrategy::Compact);
-
-            security_settings_group.set_child(security_settings_box);
-            vbox.append(security_settings_group.clone(), LayoutStrategy::Compact);
-
-            let progress_txt = Label::new("");
-            vbox.append(progress_txt.clone(), LayoutStrategy::Compact);
-
-            let mut progress_bar = ProgressBar::new();
-            progress_bar.hide();
-            progress_bar.set_value(ProgressBarValue::Indeterminate);
-            vbox.append(progress_bar.clone(), LayoutStrategy::Compact);
-
-            let do_break = Button::new("Do Break");
-            vbox.append(do_break.clone(), LayoutStrategy::Compact);
-
-            RefCell::new(UILayout {
-                do_break,
-                vbox,
-                progress_txt,
-                progress_bar,
-            })
-        });
-
-        result
-    }
-
-    fn start_fetch(&mut self) {
-        self.progress_txt.set_text("1st\n2nd");
-        self.progress_txt.show();
-        self.progress_bar.show();
-    }
-
-    fn finish_fetch(&mut self, message: &str) {
-        self.progress_txt.set_text(message);
-        self.progress_bar.hide();
     }
 }
